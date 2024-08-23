32e24559c68b9925d67a89659475f7e7f2503113
Anderson Gabriel <gabriel_13gt@hotmail.com>
2024-08-23 15:00:05 -0300

    fase2

--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@ class TextScramble {
       for (let i = 0; i < length; i++) {
         const from = oldText[i] || '';
         const to = newText[i] || '';
-        const start = Math.floor(Math.random() * 80);
-        const end = start + Math.floor(Math.random() * 80);
+        const start = Math.floor(Math.random() * 30);
+        const end = start + Math.floor(Math.random() * 30);
         this.queue.push({ from, to, start, end });
       }
   
