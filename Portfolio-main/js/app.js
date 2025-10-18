a6444f63022aefcc99d59d03cc74f67f8091bb42
Anderson Henrique de Paula Gabriel <m322980@alunos.ipb.pt>
2025-10-18 13:45:48 +0100

    Atualizando portfólio

---
+++ Portfolio-main/js/app.js
@@ -0,0 +1,100 @@
+/*class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = "!<>-_\\/[]{}—=+*^?#________";
+    this.update = this.update.bind(this);
+    this.animationComplete = false; // Variável de controle
+  }
+
+  setText(newText) {
+    if (this.animationComplete) {
+      return; // Não faça nada se a animação já foi concluída
+    }
+
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 20);
+      const end = start + Math.floor(Math.random() * 20);
+      this.queue.push({ from, to, start, end });
+    }
+
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+
+  update() {
+    if (this.animationComplete) {
+      return; // Não faça mais nada se a animação já foi concluída
+    }
+
+    let output = "";
+    let complete = 0;
+
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+
+      if (this.frame >= end) {
+        complete++;
+        output += `<span class="colored">${to}</span>`;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud colored">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+
+    this.el.innerHTML = output;
+
+    if (complete === this.queue.length) {
+      this.resolve();
+      this.animationComplete = true; // Marca a animação como concluída
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+
+const phrases = ["Anderson Gabriel"];
+// Exemplo de uso
+const el = document.querySelector(".text");
+const fx = new TextScramble(el);
+
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
+*/
+
+document
+  .getElementById("card-eletromaster")
+  .addEventListener("click", function () {
+    window.open(
+      "https://andersongabs.github.io/EletroMaster/index.html",
+      "_blank"
+    );
+  });
+
+document.getElementById("card-netflix").addEventListener("click", function () {
+  window.open("https://andersongabs.github.io/Netflix/", "_blank");
+});
